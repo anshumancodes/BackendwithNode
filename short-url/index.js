a4dd9986ed3b61c36b266e09a56d227762bdf8e6
anshumancodes <anshumanprof01@gmail.com>
2024-03-15 02:03:15 +0530

    created index.js , entry point for the execution , this handles redirection of the short id and traces acess time

---
+++ short-url/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const urlroute=require("./routes/url")
+const {ConnectToMongodb}=require("./models/connect")
+const URL=require("./models/url")
+
+ConnectToMongodb("mongodb://127.0.0.1:27017/short-url")
+.then(()=>{
+    console.log("Database Connected Successfully!");
+})
+.catch((err)=>{
+    console.error(err);
+});
+
+app.use(express.json());
+
+app.use("/url",urlroute);
+app.use('/:shortid',async (req,res) => {
+    const shortId=req.params.shortid;
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        { $push: { visitHistory: { timestamp: Date.now() } } }
+      );
+
+   res.redirect(entry.OriginalUrl);
+
+    
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
