7413b3e2037a019c0da0a950e3e74b9db519b926
anshumancodes <anshumanprof01@gmail.com>
2024-03-15 13:22:42 +0530

    added new analytics route in routes

--- short-url/routes/url.js
+++ short-url/routes/url.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {HandleGenerateShortUrl}=require("../controllers/url")
+const {HandleGenerateShortUrl,HandleClickanalytics}=require("../controllers/url")
 
 router.post("/",HandleGenerateShortUrl);
+router.get("/analytics/:shortId",HandleClickanalytics);
 
 
 module.exports=router;
