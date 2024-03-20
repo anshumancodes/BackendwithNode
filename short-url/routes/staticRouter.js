f6804f7f8bc3c56ee5afb1670e62eb75bb81512d
anshumancodes <anshumanprof01@gmail.com>
2024-03-21 01:13:38 +0530

    modified static router , passed values to Home template view

--- short-url/routes/staticRouter.js
+++ short-url/routes/staticRouter.js
@@ -1,9 +1,24 @@
+
 const express = require("express");
 const router = express.Router();
+const URL=require("../models/url")
 
 //route handling here 
 
 router.get("/",async(req,res)=>{
-    return res.render("Home");
+    if (!req.user) return res.redirect("/login");
+    const allurls = await URL.find({ createdBy:req.user._id });
+   
+    return res.render("Home", {
+        urls: allurls,
+      });
 })
+
+router.get("/signup",(req,res)=>{
+    return res.render("Signup");
+});
+router.get("/login",(req,res)=>{
+    return res.render("login");
+});
+
 module.exports=router;
