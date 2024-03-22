26e112fc3a72dc85f170fea19058c3b505794c63
anshumancodes <anshumanprof01@gmail.com>
2024-03-22 23:16:39 +0530

    instead of session id now auth service uses jwt token

--- short-url/service/auth.js
+++ short-url/service/auth.js
@@ -1,12 +1,26 @@
-const sessionIdToUserMap= new Map();
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+// Accessing the JWT secret key from process.env
+const jwt_secret_key = process.env.JWT_SECRET_KEY;
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser(user) {
+   const payload={
+    _id:user._id,
+    email:user.email
+   }
+   return jwt.sign(payload,jwt_secret_key)
 
 };
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token)return null
+    try {
+        return jwt.verify(token,jwt_secret_key); 
+        
+    } catch (error) {
+        return null
+        
+    }
 }
 
 module.exports={
